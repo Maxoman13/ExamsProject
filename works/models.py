324980fcea66dce87c1d52708d8d634e25e3501f
Maxoman13 <maxxorosh@mail.ru>
2024-06-03 20:08:35 +0300

    Подправили базу данных

--- works/models.py
+++ works/models.py
@@ -1,34 +1,33 @@
 from django.db import models
 
-# Create your models here.
 
-SERVICE_TYPE = {}
+# Create your models here.
 
 
 class Client(models.Model):
-    id = models.AutoField(primary_key=True, db_column='ClientID')
     name = models.CharField(max_length=50, db_column='ClientName')
     surname = models.CharField(max_length=50, db_column='ClientSurname')
     service = models.ForeignKey('ServiceCatalog', on_delete=models.CASCADE, db_column='ServiceNameID')
-    price = models.ManyToManyField('ServiceCatalog', through='ServicePrice', related_name='Client', db_column='ServicePriceID', verbose_name='Цена')
+    price = models.ManyToManyField('ServiceCatalog', through='ServicePrice', related_name='Client',
+                                   db_column='ServicePriceID')
     email = models.EmailField(db_column='ClientEmail')
-    tg_name = models.CharField(max_length=50, db_column='ClientTg')
-    first_date = models.DateTimeField(db_column='FirstDate')
-    update_date = models.DateTimeField(db_column='UpdateDate')
+    tg_name = models.CharField(max_length=255, db_column='ClientTg')
+    first_date = models.DateTimeField(auto_now=True, db_column='FirstDate')
+    update_date = models.DateTimeField(auto_now_add=True, db_column='UpdateDate')
     check_status = models.ForeignKey('Status', on_delete=models.CASCADE, db_column='ClientStatus')
 
     def __str__(self):
-        return f'Клиент {self.name} - {self.surname} заказал {self.service}'
+        return f'Клиент {self.name} {self.surname} заказал {self.service}'
 
     class Meta:
-        db_table = 'Service'  # имя таблицы в базе данных
-        verbose_name = 'Услуга'  # имя модели в единственном числе
-        verbose_name_plural = 'Услуги'  # имя модели во множественном числе
+        db_table = 'Service'
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
 
 
 class ServiceCatalog(models.Model):
-    id = models.AutoField(primary_key=True, db_column='ServiceNameID')
     service_name = models.CharField(max_length=50, unique=True, db_column='ServiceName')
+    slug_name = models.SlugField(max_length=255, unique=True, db_column='SlugName')
 
     class Meta:
         db_table = 'ServiceCatalog'
@@ -40,9 +39,8 @@ class ServiceCatalog(models.Model):
 
 
 class ServicePrice(models.Model):
-    id = models.AutoField(primary_key=True, db_column='ServicePriceID')
     service_name = models.ForeignKey(ServiceCatalog, on_delete=models.CASCADE, db_column='ServiceNameID')
-    service_price = models.DecimalField(max_digits=7, decimal_places=2, db_column='ServicePrice')
+    service_price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, db_column='ServicePrice')
     service_client = models.ForeignKey(Client, on_delete=models.CASCADE, db_column='ClientID')
 
     class Meta:
@@ -51,12 +49,16 @@ class ServicePrice(models.Model):
         verbose_name_plural = 'Цены услуг'
 
     def __str__(self):
-        return f'Цена услуги {self.service_name}'
+        return f'Цена услуги {self.service_name} для {self.service_client}'
 
 
 class Status(models.Model):
-    id = models.AutoField(primary_key=True, db_column='StatusID')
-    status_name = models.CharField(max_length=50, db_column='StatusName')
+    status_name = models.CharField(max_length=255, db_column='StatusName')
+
+    class Meta:
+        db_table = 'Status'
+        verbose_name = 'Статус услуги'
+        verbose_name_plural = 'Статус услуг'
 
     def __str__(self):
         return f'Статус {self.status_name}'
