b720e09aed5ae662d60147d1422e17b2415f83a8
Maxoman13 <maxxorosh@mail.ru>
2024-06-29 00:06:08 +0300

    Telegramm bot

---
+++ works/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import asyncio
+
+from master.settings import TELEGRAM_BOT_TOKEN, YOUR_PERSONAL_CHAT_ID
+from .models import Client
+from .telegram_bot import send_telegram_message
+import os
+
+
+@receiver(post_save, sender=Client)
+def notify_admin(sender, instance, created, **kwargs):
+    if created:
+        message = f'Новый клиент: {instance.name} {instance.surname} была добавлена.'
+        asyncio.run(send_telegram_message(TELEGRAM_BOT_TOKEN, YOUR_PERSONAL_CHAT_ID, message))
